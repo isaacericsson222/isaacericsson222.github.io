86364491cef7f2382c5a91b08b01c1975b75cdbd
isaacericsson222 <isaac.h.ericsson@gmail.com>
2026-01-11 16:59:35 -0800

    more terminal changes

--- intergalactic-inclination/src/scripts/terminal.js
+++ intergalactic-inclination/src/scripts/terminal.js
@@ -8,9 +8,22 @@ window.addEventListener("DOMContentLoaded", () => {
   const ANSI = {
     reset: "\x1b[0m",
     bold: "\x1b[1m",
-    cyan: "\x1b[36m",
+    dim: "\x1b[2m",
+    italic: "\x1b[3m",
+    underline: "\x1b[4m",
+    blink: "\x1b[5m",
+    inverse: "\x1b[7m",
+    hidden: "\x1b[8m",
+    strike: "\x1b[9m",
+
+    black: "\x1b[30m",
+    red: "\x1b[31m",
     green: "\x1b[32m",
-    gray: "\x1b[90m",
+    yellow: "\x1b[33m",
+    blue: "\x1b[34m",
+    magenta: "\x1b[35m",
+    cyan: "\x1b[36m",
+    white: "\x1b[37m",
   };
 
   const term = new Terminal({
@@ -38,21 +51,39 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const prompt = () => term.write(PROMPT);
 
+  const printHelp = () => {
+    term.writeln(
+      `${ANSI.bold}${ANSI.magenta}help${ANSI.reset}      Prints this help message`
+    );
+    term.writeln(
+      `${ANSI.bold}${ANSI.magenta}ls${ANSI.reset}        Prints a fake directory structure`
+    );
+    term.writeln(
+      `${ANSI.bold}${ANSI.magenta}about${ANSI.reset}     About this site`
+    );
+    term.writeln(
+      `${ANSI.bold}${ANSI.magenta}clear${ANSI.reset}     Clears the screen`
+    );
+  };
+
   const runCommand = (cmd) => {
     switch (cmd) {
       case "":
         return;
       case "help":
-        term.writeln("Commands: help, clear, about");
+        printHelp();
         break;
       case "clear":
         term.clear();
+        break;
+      case "ls":
+        term.writeln(`${ANSI.green}Hi\r\nthere\r${ANSI.reset}`);
         break;
       case "about":
         term.writeln("Isaac Ericsson — CS @ Cal Poly SLO");
         break;
       default:
-        term.writeln(`command not found: ${cmd}`);
+        term.writeln(`${ANSI.gray}command not found:${ANSI.reset} ${cmd}`);
     }
   };
 
